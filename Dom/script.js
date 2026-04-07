00958e04d0355c9ef6941e51098fc093f350574b
AnkitHeree <ankityadav9082384811@gmail.com>
2026-04-07 14:39:00 +0530

    Dom

---
+++ Dom/script.js
@@ -0,0 +1,24 @@
+// let hee = document.getElementById("h");
+// let ankit = document.getElementById("ankit");
+// ankit.innerHTML = hee.innerHTML
+// console.log(hee.innerHTML)
+// console.log(hee.innerText)
+
+// let paragraph = document.getElementsByTagName("p")
+
+// console.log(paragraph [0])
+// console.log(paragraph [1])
+// console.log(paragraph [2])
+
+
+// let clasname = document.getElementsByClassName("hel")
+// console.log(clasname[0])
+// console.log(clasname[1])
+// console.log(clasname[2])
+
+
+let querryAll = document.querySelectorAll("p");
+querryAll.forEach((item,index)=>{
+console.log(item.innerHTML)
+});
+console.log(querryAll)
